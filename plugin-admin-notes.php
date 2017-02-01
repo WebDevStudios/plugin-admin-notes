e06de095a3edc61e99b6264b07e65c63997bddfe
Gary Kovar <gkovar@gmail.com>
2017-01-31 22:52:18 -0500

    Adds awareness of when we are uninstalling.

--- plugin-admin-notes.php
+++ plugin-admin-notes.php
@@ -202,8 +202,12 @@ final class WDS_Plugin_Police {
 		// < 10 for CPT_Core,
 		// < 5 for Taxonomy_Core,
 		// 0 Widgets because widgets_init runs at init priority 1.
-		add_action( 'plugins_loaded', array( $this, 'init' ),1 );
+		add_action( 'plugins_loaded', array( $this, 'init' ), 1 );
 		add_action( 'admin_enqueue_scripts', array( $this, 'eq_scripts' ) );
+
+		if ( WP_UNINSTALL_PLUGIN && isset($_POST['action']) && 'delete-plugin' == $_POST['action']) {
+
+		}
 	}
 
 	/**
@@ -436,7 +440,7 @@ function wds_plugin_police() {
 }
 
 // Kick it off.
-add_action( 'plugins_loaded', array( wds_plugin_police(), 'hooks' ),0 );
+add_action( 'plugins_loaded', array( wds_plugin_police(), 'hooks' ), 0 );
 
 register_activation_hook( __FILE__, array( wds_plugin_police(), '_activate' ) );
 register_deactivation_hook( __FILE__, array( wds_plugin_police(), '_deactivate' ) );
